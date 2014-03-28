734753064a3499c1acd6a5445e4c5a65690fb219
Alex Martin <alex.john.martin@gmail.com>
2014-03-27 21:11:12 -0700

    adding Nokia Imaging SDK, making edit page apply a basic cartoon filter

--- Toonify/EditImagePage.xaml.cs
+++ Toonify/EditImagePage.xaml.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using Nokia.Graphics.Imaging;
+using Nokia.InteropServices.WindowsRuntime;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using Microsoft.Xna.Framework.Media;
-using System.Windows.Media.Imaging;
 
 namespace Toonify
 {
     public partial class EditImagePage : PhoneApplicationPage
     {
+        private FilterEffect _cartoonEffect = null;
+        private WriteableBitmap _cartoonImageBitmap = null;
+        private WriteableBitmap _thumbnailImageBitmap = null;
+
         public EditImagePage()
         {
             InitializeComponent();
@@ -30,7 +38,7 @@ namespace Toonify
             {
                 if (source.MediaSourceType == MediaSourceType.LocalDevice)
                 {
-                    var mediaLibrary = new MediaLibrary(source);
+                    var mediaLibrary = new MediaLibrary(source);    
                     PictureAlbumCollection allAlbums = mediaLibrary.RootPictureAlbum.Albums;
                     foreach (PictureAlbum album in allAlbums)
                     {
@@ -39,13 +47,50 @@ namespace Toonify
                             var pic = album.Pictures.Where(p => p.Name.Equals(imageName, StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault(); 
                             if (pic != null)
                             {
-                                BitmapImage b = new BitmapImage();
-                                b.SetSource(pic.GetImage());
-                                ImageDisplay.Source = b; 
+                                //BitmapImage b = new BitmapImage();
+                                //b.SetSource(pic.GetImage());
+                                //ImageDisplay.Source = b; 
+                                CreateCartoonImage(pic.GetImage()); 
                             }
                         }
                     }
                 }
+            }
+        }
+
+        private async void CreateCartoonImage(Stream chosenPhoto)
+        {
+            _cartoonImageBitmap = new WriteableBitmap((int)ImageDisplay.Width, (int)ImageDisplay.Height);
+            _thumbnailImageBitmap = new WriteableBitmap((int)ImageDisplay.Width, (int)ImageDisplay.Height);
+
+            try
+            {
+                // Show thumbnail of original image.
+                _thumbnailImageBitmap.SetSource(chosenPhoto);
+                //ImageDisplay.Source = _thumbnailImageBitmap;
+
+                // Rewind stream to start.                     
+                chosenPhoto.Position = 0;
+
+                // A cartoon effect is initialized with selected image stream as source.
+                var imageStream = new StreamImageSource(chosenPhoto);
+                _cartoonEffect = new FilterEffect(imageStream);
+
+                // Add the cartoon filter as the only filter for the effect.
+                var cartoonFilter = new CartoonFilter();
+                _cartoonEffect.Filters = new[] { cartoonFilter };
+
+                // Render the image to a WriteableBitmap.
+                var renderer = new WriteableBitmapRenderer(_cartoonEffect, _thumbnailImageBitmap);
+                _cartoonImageBitmap = await renderer.RenderAsync();
+
+                // Set the rendered image as source for the cartoon image control.
+                ImageDisplay.Source = _cartoonImageBitmap;
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                return;
             }
         }
     }
